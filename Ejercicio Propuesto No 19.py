205b9509fe143350e3bc95c41817b45f9da60468
Juan Grajales A. <jgrajalesa@unal.edu.co>
2024-11-13 15:09:19 -0500

    Corrección Ejercicio 19

--- Ejercicio Propuesto No 19.py
+++ Ejercicio Propuesto No 19.py
@@ -1,17 +1,20 @@
-tamaño_lado_triangulo = (input("Ingrese el tamaño de un lado del triangulo equilatero: "))
+import math
+
+tamaño_lado_triangulo = input("Ingrese el tamaño de un lado del triángulo equilátero: ")
+
 try:
-  tamaño_lado_triangulo = float(tamaño_lado_triangulo)
-  if tamaño_lado_triangulo >= 0:
+    tamaño_lado_triangulo = float(tamaño_lado_triangulo)
+    
+    if tamaño_lado_triangulo > 0:
+        perimetro = 3 * tamaño_lado_triangulo
+        valor_altura = (math.sqrt(3) / 2) * tamaño_lado_triangulo
+        area = (math.sqrt(3) / 4) * tamaño_lado_triangulo ** 2
 
-    perimetro = 3 * tamaño_lado_triangulo
-    valor_altura = tamaño_lado_triangulo * 3/2
-    area = tamaño_lado_triangulo**2 * 1.73
-
-    print(f"Perimetro: {perimetro:.2f}")
-    print(f"Valor de la altura: {valor_altura:.2f}")
-    print(f"Área: {area:.2f}")
-  else: 
-      print("El número debe ser positivo")
+        print(f"El perímetro es: {perimetro:.2f}")
+        print(f"El valor de la altura es: {valor_altura:.2f}")
+        print(f"El valor del área es: {area:.2f}")
+    else: 
+        print("El número debe ser positivo.")
 except ValueError:
     print("Por favor, ingrese un número válido.")
-    print("En caso de que su número sea un decimal, utilice un punto.")
+    print("En caso de que su número sea decimal, utilice un punto.")
