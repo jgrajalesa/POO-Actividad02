a749c41818a8b4f2806cebdc42dd6d52da3f6111
pablovanegas <104100454+pablovanegas@users.noreply.github.com>
2024-11-13 23:26:09 -0500

    Add files via upload

---
+++ Ejercicio Propuesto 4.7.py
@@ -0,0 +1,10 @@
+A = float(input("Ingrese el valor de A: "))
+B = float(input("Ingrese el valor de B: "))
+
+# Comparar los valores de A y B y mostrar el mensaje correspondiente
+if A > B:
+    print("A es mayor que B")
+elif A < B:
+    print("A es menor que B")
+else:
+    print("A es igual a B")
